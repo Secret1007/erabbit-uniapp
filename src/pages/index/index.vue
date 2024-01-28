54196592f7c1255a2c0b4f04e8d520cef6ca93bc
mecrev-secret <18364307619@163.com>
2024-01-28 16:34:42 +0800

    添加 猜你喜欢模块

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,11 +2,12 @@
 import XtxSwiper from '@/components/XtxSwiper.vue'
 import CategoryPanel from './components/CategoryPanel.vue'
 import customNavBar from './components/customNavBar.vue'
-import HotPanel from './components/HotPanel.vue'
+import HotPanel from './components/HotPane.vue'
 import { ref } from 'vue'
 import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services'
 import { onLoad } from '@dcloudio/uni-app'
+import type { XtxGuessInstance } from '@/types/components'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -29,6 +30,26 @@ const getHomeHotData = async () => {
   hotList.value = res.result
 }
 
+// 获取猜你喜欢组件实例
+const guessRef = ref<XtxGuessInstance>()
+
+// 滚动触底事件
+const onScrolltolower = () => {
+  guessRef.value?.getMore()
+}
+
+// 下拉刷新状态
+const isTriggered = ref(false)
+// 自定义下拉刷新被触发
+const onRefresherrefresh = async () => {
+  // 开启动画
+  isTriggered.value = true
+  // 重置猜你喜欢组件数据
+  guessRef.value?.resetData() // 加载数据
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()]) // 关闭动画
+  isTriggered.value = false
+}
+
 // 页面加载
 onLoad(() => {
   getHomeBannerData()
@@ -39,11 +60,29 @@ onLoad(() => {
 
 <template>
   <customNavBar />
-  <XtxSwiper :list="bannerList" />
-  <CategoryPanel :list="categoryList" />
-  <HotPanel :list="hotList" />
+  <scroll-view
+    scroll-y
+    class="scroll-view"
+    @scrolltolower="onScrolltolower"
+    refresher-enabled
+    @refresherrefresh="onRefresherrefresh"
+    :refresher-triggered="isTriggered"
+  >
+    <XtxSwiper :list="bannerList" />
+    <CategoryPanel :list="categoryList" />
+    <HotPanel :list="hotList" />
+    <XtxGuess ref="guessRef" />
+  </scroll-view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  overflow: hidden;
+}
+.scroll-view {
+  flex: 1;
+  overflow: hidden;
+}
 </style>
--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -5,8 +5,13 @@
  */
 import 'vue'
 import XtxSwiper from './XtxSwiper.vue'
+import XtxGuessVue from '/XtxGuess.vue'
+
 declare module 'vue' {
   export interface GlobalComponents {
     XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuessVue
   }
 }
+// 组件实例类型
+export type XtxGuessInstance = InstanceType<typeof XtxGuess>
