99308cff30ab9d2d9e7198b913504c097321a3a7
mecrev-secret <18364307619@163.com>
2024-01-28 08:59:52 +0800

    add categoryPanel

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
-// import XtxSwiper from '@/components/XtxSwiper.vue';
+import XtxSwiper from '@/components/XtxSwiper.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import customNavBar from './components/customNavBar.vue'
+import { ref } from 'vue'
+import type { BannerItem, CategoryItem } from '@/types/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services'
+import { onLoad } from '@dcloudio/uni-app'
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 获取前台分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
+// 页面加载
+onLoad(() => {
+  getHomeBannerData()
+  getHomeCategoryData()
+})
 </script>
 
 <template>
   <customNavBar />
-  <!-- <view class="index">index</view> -->
-  <XtxSwiper />
+  <XtxSwiper :list="bannerList" />
+  <CategoryPanel :list="categoryList" />
 </template>
 
 <style lang="scss">
